502ec381c75e019d32b949e248f7c1dddd381f9a
james rhew <psd700@gmail.com>
2017-03-28 10:03:44 -0600

    Gti push test

---
+++ index.js
@@ -0,0 +1,7 @@
+var b=document.createElement('h3');
+ var c=document.createTextNode("World");
+ b.setAttribute('id','first');
+
+ b.appendChild(c);
+ document.body.appendChild(b);
+ b.innerHTML+= " hello";
